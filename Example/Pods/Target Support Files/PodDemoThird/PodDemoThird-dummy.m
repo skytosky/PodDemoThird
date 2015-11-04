cd1dc562db2fc4b6b715ced87be328dc25c6dc23
467504109@qq.com <467504109@qq.com>
2015-11-04 11:43:15 +0800

    0.1.0

---
+++ Example/Pods/Target Support Files/PodDemoThird/PodDemoThird-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_PodDemoThird : NSObject
+@end
+@implementation PodsDummy_PodDemoThird
+@end
---
+++ Example/Pods/Target Support Files/Pods-PodDemoThird_Example/Pods-PodDemoThird_Example-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_PodDemoThird_ExampleVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_PodDemoThird_ExampleVersionString[];
+
